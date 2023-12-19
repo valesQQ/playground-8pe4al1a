f9e2c8a857daf7d09bf2802e2834aaf213f86bcb
valesQQ <43030360+valesQQ@users.noreply.github.com>
2023-12-19 16:28:48 +0100

    Update the exercice

--- rust-project/src/lib.rs
+++ rust-project/src/lib.rs
@@ -1,9 +1,5 @@
 pub fn count_all_stars(galaxies: &[i32]) -> i32 {
     let mut total_stars = 0;
 
-    for &stars in galaxies {
-        total_stars = stars; // fix me
-    }
-
-    total_stars
+    // Complete the code
 }
