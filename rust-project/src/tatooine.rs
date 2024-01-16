54fbfae00697e272c6d2f56e33e265152e88a50c
Valentin Schrub <v.schrub@technologyandstrategy.com>
2024-01-16 13:29:50 +0100

    Cleanup and change hints

--- rust-project/src/tatooine.rs
+++ rust-project/src/tatooine.rs
@@ -1,4 +1,8 @@
-/// count the nunber of systems that have two suns
+/*
+Luke has lost his star map and wants to go home.
+He has access to a list containing the number of suns in each solar system of the galaxy (the function input).
+How many of the solar systems could be home to Tatooine ?
+*/
 pub fn find_tatooine(solar_systems: &[i32]) -> i32 {
     0
 }
