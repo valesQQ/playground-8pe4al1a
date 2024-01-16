6a729d39d8fe5d262d9c292ca161681f4b08265a
valesQQ <43030360+valesQQ@users.noreply.github.com>
2024-01-16 13:49:11 +0100

    Ultra giga obvious hint

--- rust-project/src/tatooine.rs
+++ rust-project/src/tatooine.rs
@@ -2,6 +2,8 @@
 Luke has lost his star map and wants to go home.
 He has access to a list containing the number of suns in each solar system of the galaxy (the function input).
 How many of the solar systems could be home to Tatooine ?
+
+Hint: https://starwars.fandom.com/wiki/Tatoo_system
 */
 pub fn find_tatooine(solar_systems: &[i32]) -> i32 {
     0
